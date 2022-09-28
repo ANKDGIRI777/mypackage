d5620e576edb6dd48f39d7fb55550528dbce1ba0
Aniket D. Giri <aniket.g@safe.security>
2022-09-28 16:47:33 +0530

    feat: to add the changes to npm

--- src/tsclasses.ts
+++ src/tsclasses.ts
@@ -11,6 +11,6 @@ class Person {
 }
 
 const person = new Person(
-  "Jdasdaadasdasdsadhasidghwaifywosfhsal;dfgnas;dlfnaops;fturftwe'priuw'epofjkpw'ekf'w;ekf;'ekf['pewkf['pwekf[paeskf[sdasane"
+  "Jdasdaadasdasdsadhasidghwaifywosfhsal;dfgnas;dlfnadhafnal;ksfnoa;gjtp'wajfgp'awjgp'wajg'pwagjkp'rwjgk'prwjg'apwjgprjgpo;rjgo;irrjgoilrsjgo;isrrgjaops;fturftwe'priuw'epofjkpw'ekf'w;ekf;'ekf['pewkf['pwekf[paeskf[sdasane"
 );
 console.log(person.getName());
